7da67196af04f935646ecd02e67e018fd3a5beea
lilydjwg <lilydjwg@gmail.com>
2024-08-29 10:45:52 +0800

    support copying with text fragment
    https://developer.mozilla.org/en-US/docs/Web/URI/Fragment/Text_fragments

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 const RootMenuId = 'copy-link-address@lilydjwg.me'
+const RootMenuIdText = 'copy-link-address+text@lilydjwg.me'
 
 browser.contextMenus.create({
   id: RootMenuId,
@@ -6,6 +7,19 @@ browser.contextMenus.create({
   contexts: ['link'],
 })
 
-browser.contextMenus.onClicked.addListener(function(info, tab) {
-  navigator.clipboard.writeText(info.linkUrl)
+browser.contextMenus.create({
+  id: RootMenuIdText,
+  title: browser.i18n.getMessage('contextMenuSelectionTitle'),
+  contexts: ['selection'],
 })
+
+browser.contextMenus.onClicked.addListener(function(info, tab) {
+  if(info.menuItemId == RootMenuId) {
+    navigator.clipboard.writeText(info.linkUrl)
+  } else if(info.menuItemId == RootMenuIdText) {
+    let url = new URL(tab.url)
+    url.hash = ':~:text=' + encodeURIComponent(info.selectionText)
+    navigator.clipboard.writeText(url.toString())
+  }
+})
+
