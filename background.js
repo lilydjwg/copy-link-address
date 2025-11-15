04a1daf6a64a98722ff1fffb236204fb8cdb967c
lilydjwg <lilydjwg@gmail.com>
2025-11-15 12:16:06 +0800

    remove text fragment support as Firefox now supports it natively
    and Firefox does it much better.
    
    Removing it makes sure that a single menu is added by this extension and
    thus `A` always works, even on selection on a link.

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 const RootMenuId = 'copy-link-address@lilydjwg.me'
-const RootMenuIdText = 'copy-link-address+text@lilydjwg.me'
 
 browser.contextMenus.create({
   id: RootMenuId,
@@ -7,20 +6,6 @@ browser.contextMenus.create({
   contexts: ['link'],
 })
 
-browser.contextMenus.create({
-  id: RootMenuIdText,
-  title: browser.i18n.getMessage('contextMenuSelectionTitle'),
-  contexts: ['selection'],
-})
-
 browser.contextMenus.onClicked.addListener(function(info, tab) {
-  if(info.menuItemId == RootMenuId) {
-    navigator.clipboard.writeText(info.linkUrl)
-  } else if(info.menuItemId == RootMenuIdText) {
-    let url = new URL(tab.url)
-    // Google Chrome requires to escape - too
-    url.hash = ':~:text=' + encodeURIComponent(info.selectionText).replace(/-/g, '%2D')
-    navigator.clipboard.writeText(url.toString())
-  }
+  navigator.clipboard.writeText(info.linkUrl)
 })
-
